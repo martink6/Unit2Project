6c1ae2c48ec1332ae88de80576a4f8439f5f3ec0
BT_2N2_17 <a@b.c>
2023-10-16 14:04:40 -0400

    Init

---
+++ src/LinearEquation.java
@@ -0,0 +1,11 @@
+public class LinearEquation {
+    private double xOne;
+    private double yOne;
+    private double xTwo;
+    private double yTwo;
+
+    public LinearEquation(double xOne, double yOne, double xTwo, double yTwo) {
+        this.xOne = xOne; this.yOne = yOne;
+        this.xTwo = xTwo; this.yTwo = yTwo;
+    }
+}
