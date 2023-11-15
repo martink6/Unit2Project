b951306ebb4c6ba154038ada0510e934178a2209
martin <a>
2023-11-14 23:44:19 -0500

    Done

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,9 +5,125 @@ public class LinearEquation {
     private double yTwo;
 
     public LinearEquation(double xOne, double yOne, double xTwo, double yTwo) {
-        this.xOne = xOne; this.yOne = yOne;
-        this.xTwo = xTwo; this.yTwo = yTwo;
+        this.xOne = xOne;
+        this.yOne = yOne;
+        this.xTwo = xTwo;
+        this.yTwo = yTwo;
     }
 
+    public void setCoordinates(double xOne, double yOne, double xTwo, double yTwo) {
+        this.xOne = xOne;
+        this.yOne = yOne;
+        this.xTwo = xTwo;
+        this.yTwo = yTwo;
+    }
 
+    private double roundedToHundredth(double toRound) {
+        return Math.round(toRound * 100.0) / 100.0;
+    }
+
+    public double distance() {
+        double dist = Math.sqrt(Math.pow(xTwo - xOne, 2) + Math.pow(yTwo - yOne, 2));
+        return roundedToHundredth(dist);
+    }
+
+    public double slope() {
+        if (xOne == xTwo) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return roundedToHundredth(yTwo - yOne) / (xTwo - xOne);
+    }
+
+    public double yIntercept() {
+        double m = slope();
+        return roundedToHundredth(yOne - m * xOne);
+    }
+
+    private int gcd(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return gcd(n2, n1 % n2);
+    }
+
+    public String equation() {
+        double m = slope();
+        double b = yIntercept();
+
+        String slope = formatSlope(m);
+        String intercept = formatIntercept(b);
+
+        if (slope.contains("/1")) {
+            slope = slope.substring(0, slope.length() - 2);
+        }
+
+        if (slope.equals("1")) {
+            slope = "";
+        }
+
+        return String.format("y = %sx%s", slope, intercept);
+    }
+
+    private String formatSlope(double slope) {
+        if (Double.isInfinite(slope)) {
+            return "undefined";
+        }
+
+        if (slope == 0) {
+            return "0";
+        }
+
+        int numerator = (int) ((yTwo - yOne) * 1000);
+        int denominator = (int) ((xTwo - xOne) * 1000);
+
+        int gcdValue = gcd(Math.abs(numerator), Math.abs(denominator));
+        numerator /= gcdValue;
+        denominator /= gcdValue;
+
+        if (denominator == 1) {
+            return String.valueOf(numerator);
+        }
+
+        if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
+        }
+
+        if (denominator < 0) {
+            denominator = Math.abs(denominator);
+            numerator *= -1;
+        }
+
+        return numerator + "/" + denominator;
+    }
+
+    private String formatIntercept(double intercept) {
+        if (intercept == 0) {
+            return "";
+        }
+        if (intercept < 0) {
+            return " - " + Math.abs(intercept);
+        }
+
+        return " + " + intercept;
+    }
+
+    public String coordinateForX(double x) {
+        double m = slope();
+        double b = yIntercept();
+
+        double y = m * x + b;
+
+        return String.format("(%f, %f)", roundedToHundredth(x), roundedToHundredth(y));
+    }
+
+    public String lineInfo() {
+        return String.format("""
+                The two points are (%.0f, %.0f) and (%.0f, %.0f).
+                The equation of the line between these points is %s.
+                The slope of this line is %.2f.
+                The y-intercept of this line is %.2f.
+                The distance between these points is %.2f.
+                """, xOne, yOne, xTwo, yTwo, equation(), slope(), yIntercept(), distance());
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,73 @@
-public class LinearEquationLogic {
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class  LinearEquationLogic {
+    Scanner scan = new Scanner(System.in);
+    public void start() {
+        System.out.println("Welcome to the Linear Equation Calculator!");
+        System.out.println("enter the coordinates of two points on a line, ex: (1, 2) or (3, 4)");
+
+        boolean cont = true;
+
+        LinearEquation equation = new LinearEquation(0, 0, 0, 0);
+
+        while (cont) {
+            try {
+                int[] coords1 = parseCoordinatePair(askQuestion("Enter the first coordinate pair: "));
+                int[] coords2 = parseCoordinatePair(askQuestion("Enter the second coordinate pair: "));
+
+                if (coords1[0] == coords2[0] && coords1[1] == coords2[1]) {
+                    throw new IllegalArgumentException("The two points must be different.");
+                }
+                if (coords1[0] == coords2[0]) {
+                    throw new IllegalArgumentException("The two points must not have the same x-coordinate.");
+                }
+
+                equation.setCoordinates(coords1[0], coords1[1], coords2[0], coords2[1]);
+                System.out.println(equation.lineInfo());
+
+                String cordX = askQuestion("Enter a value for x: ");
+                System.out.println("Coordinate for x: " + equation.coordinateForX(stringToDouble(cordX)));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            String contStr = askQuestion("Would you like to continue? (y/n) ");
+            if (contStr.equals("n")) cont = false;
+        }
+    }
+
+    private int[] parseCoordinatePair(String input) {
+        int numOne = 0; int numTwo = 0;
+        Pattern pattern = Pattern.compile("\\((-?\\d+),\\s*(-?\\d+)\\)");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            numOne = stringToInt(matcher.group(1));
+            numTwo = stringToInt(matcher.group(2));
+        }
+
+        return new int[] {numOne, numTwo};
+    }
+
+    private String askQuestion(String question) {
+        System.out.print(question);
+        return scan.nextLine();
+    }
+
+    private int stringToInt(String num) {
+        try {
+            return Integer.parseInt(num);
+        } catch (Exception e) {
+            System.out.printf("[WARN] Invalid input \"%s\" to be converted to integer.%n", num);
+        }
+        return 0;
+    }
+
+    private double stringToDouble(String num) {
+        try {
+            return Double.parseDouble(num);
+        } catch (Exception e) {
+            System.out.printf("[WARN] Invalid input \"%s\" to be converted to double.%n", num);
+        }
+        return 0;
+    }
 }
